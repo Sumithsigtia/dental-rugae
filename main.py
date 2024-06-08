66fb44d35753c3455ab04fd87063dd92c0036c80
Sumithsigtia <137398119+Sumithsigtia@users.noreply.github.com>
2024-06-08 13:38:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,15 @@ import numpy as np
 from tensorflow.keras.preprocessing import image as keras_image
 from tensorflow.keras.models import load_model
 
+
 # Function to load and preprocess image
 def preprocess_image(img_path, target_size):
     img = Image.open(img_path)
     img = img.resize(target_size)
     img = keras_image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
-    img /= 224.0
+    img /= 255.0
     return img
-
-# Function to resize and display image
-def resize_and_display_image(img_path, target_size):
-    img = Image.open(img_path)
-    img = img.resize(target_size)
-    st.image(img, caption='Resized Image', use_column_width=True)
 
 # Function to make prediction
 def predict_image(img, model, categories):
@@ -43,12 +38,10 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     # Display uploaded image
     image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
 
     # Preprocess image
-    img = preprocess_image(uploaded_file, (224, 224))
-
-    # Resize and display image
-    resize_and_display_image(uploaded_file, (224, 224))
+    img = preprocess_image(uploaded_file, (128, 128))
 
     # Make prediction
     predicted_class, confidence = predict_image(img, model, categories)
