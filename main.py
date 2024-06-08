c27941fa1c7420466b9d082525584d2a2515ae52
Sumithsigtia <137398119+Sumithsigtia@users.noreply.github.com>
2024-06-08 13:29:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,6 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     # Display uploaded image
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
 
     # Preprocess image
     img = preprocess_image(uploaded_file, (224, 224))
